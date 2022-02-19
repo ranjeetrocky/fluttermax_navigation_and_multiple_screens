52bfd9f57aafa8fde29b602291bd88b986af7d47
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-19 20:50:52 +0530

    Meal item completed

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -14,6 +14,28 @@ class MealItem extends StatelessWidget {
     required this.complexity,
     required this.affordability,
   }) : super(key: key);
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+    }
+  }
+
   void _selectMeal() {}
 
   @override
@@ -25,44 +47,111 @@ class MealItem extends StatelessWidget {
       child: InkWell(
         onTap: _selectMeal,
         borderRadius: BorderRadius.circular(15),
-        child: Column(children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15)),
-                child: Image.network(
-                  imgUrl,
-                  height: 250,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                  loadingBuilder: (context, child, imageChunkEvent) {
-                    // print(imageChunkEvent?.expectedTotalBytes!.toString());
-                    // print("\n" +
-                    //     (imageChunkEvent?.cumulativeBytesLoaded).toString());
-                    // Future.delayed(const Duration(seconds: 1), () {
-                    //   print("bruh");
-                    // });
-                    return imageChunkEvent == null
-                        ? child
-                        : SizedBox(
-                            height: 250,
-                            child: Center(
-                              child: CircularProgressIndicator.adaptive(
-                                strokeWidth: 1,
-                                semanticsLabel: "Loading",
-                                value: (imageChunkEvent.cumulativeBytesLoaded /
-                                    imageChunkEvent.expectedTotalBytes!),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Image.network(
+                    imgUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (context, child, imageChunkEvent) {
+                      // print(imageChunkEvent?.expectedTotalBytes!.toString());
+                      // print("\n" +
+                      //     (imageChunkEvent?.cumulativeBytesLoaded).toString());
+                      // Future.delayed(const Duration(seconds: 1), () {
+                      //   print("bruh");
+                      // });
+                      return imageChunkEvent == null
+                          ? child
+                          : SizedBox(
+                              height: 250,
+                              child: Center(
+                                child: CircularProgressIndicator.adaptive(
+                                  strokeWidth: 1,
+                                  semanticsLabel: "Loading",
+                                  value:
+                                      (imageChunkEvent.cumulativeBytesLoaded /
+                                          imageChunkEvent.expectedTotalBytes!),
+                                ),
                               ),
-                            ),
-                          );
-                  },
+                            );
+                    },
+                  ),
                 ),
-              )
-            ],
-          )
-        ]),
+                Positioned(
+                    child: Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15)),
+                    gradient: LinearGradient(colors: [
+                      Colors.black.withAlpha(210),
+                      Colors.black.withAlpha(0),
+                    ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+                  ),
+                )),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    width: 382.8,
+                    // width: 382.8,
+                    // width: constraints.maxWidth,
+                    child: Text(
+                      title,
+                      style: const TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      // maxLines: 1,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityText)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work_outline_rounded),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityText)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min')
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
