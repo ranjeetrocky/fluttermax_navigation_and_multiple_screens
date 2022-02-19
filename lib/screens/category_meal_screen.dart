6852e1faf712b5b182ffde6b77ab24c99aeeeb74
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-19 16:57:06 +0530

    recieved meal items

--- lib/screens/category_meal_screen.dart
+++ lib/screens/category_meal_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttermax_navigation_and_multiple_screens/dummy_data.dart';
 
 class CategoryMealScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -8,12 +9,20 @@ class CategoryMealScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    var categoryTitle = routeArgs['title'] as String;
+    var categoryId = routeArgs['id'] as String;
+    final categoryMeals = kDummyMeals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text(routeArgs['title'] as String),
+        title: Text(categoryTitle),
       ),
-      body: const Center(
-        child: Text("The Recipes for the Category!"),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
