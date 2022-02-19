c483739eec9a469838e0ffb5dd6b1b4b85ff5fb6
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-19 14:11:21 +0530

    boilerPlate | initial commit

---
+++ android/app/src/main/kotlin/com/example/fluttermax_navigation_and_multiple_screens/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttermax_navigation_and_multiple_screens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation & Multiple Screen Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daily Meals"),
+      ),
+      body: const Center(
+          child: Text(
+              "Navigation Time!")), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
